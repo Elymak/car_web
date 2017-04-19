8a50a29578373b7612623332e96da8b67dbdc648
Serial <Serial@192.168.1.25>
2017-04-19 21:14:21 +0200

    Ajout interface biblio + bean

---
+++ src/main/java/car/tp4/entity/Bibliotheque.java
@@ -0,0 +1,71 @@
+package car.tp4.entity;
+
+import java.awt.RenderingHints.Key;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import javax.ejb.Stateful;
+
+@Stateful(name="maBibliotheque")
+public class Bibliotheque implements BibliothequeItf {
+
+	private Map<Book, Integer> books;
+	
+	public Bibliotheque() {
+		books = new HashMap<Book, Integer>();
+	}
+		
+	
+	@Override
+	public void init() {
+		books.put(new Book("J.K. Rolling", "Harry Potter", 1997), 3);
+		books.put(new Book("J.R.R. Tolkien", "The Lord of the rings", 1937), 6);
+		books.put(new Book("Georges R.R. Martin", "Game Of Thrones", 2015), 0);
+	}
+
+	@Override
+	public boolean createBook(String titre, String auteur, int annee) {
+		for(Map.Entry<Book, Integer> entry : books.entrySet()){
+			Book b = entry.getKey();
+			if(b.getAuthor().equals(auteur) && b.getTitle().equals(titre) && b.getYear() == annee){
+				addbook(b);
+				return false;
+			}
+		}
+		books.put(new Book(titre, auteur, annee), 1);
+		return true;
+	}
+	
+	@Override
+	public void addbook(Book b){
+		books.put(b, books.get(b) +1);
+	}
+
+	@Override
+	public boolean editBook(Book b, String titre, String auteur, int annee) {
+		Set<Book> set = books.keySet();
+		for(Book book : set){
+			if(book.equals(b)){
+				book.setAuthor(auteur);
+				book.setTitle(titre);
+				book.setYear(annee);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public Set<Book> listBooks() {
+		return books.keySet();
+	}
+
+	@Override
+	public List<String> listAuteur() {
+		return null;
+	}
+}
---
+++ src/main/java/car/tp4/entity/BibliothequeItf.java
@@ -0,0 +1,16 @@
+package car.tp4.entity;
+
+import java.util.List;
+import java.util.Set;
+
+import javax.ejb.Local;
+
+@Local
+public interface BibliothequeItf {
+	public void init();
+	public void addbook(Book b);
+	public boolean createBook(String titre, String auteur, int annee);
+	public boolean editBook(Book b, String titre, String auteur, int annee);
+	public Set<Book> listBooks();
+	public List<String> listAuteur();
+}
